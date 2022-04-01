42dead4baee1c409e06e84cabfc8502c09250357
Raúl Marín <raulmarin.informatica@iespacomolla.es>
2022-04-01 17:39:30 +0200

    Tabs

--- xarxa-web/src/components/alumno.component.js
+++ xarxa-web/src/components/alumno.component.js
@@ -12,12 +12,20 @@ class Alumno extends Component {
         nombre: "",
         apellidos: "",
       },
-      message: "",
+      cursoIndex: 0,
     };
   }
   componentDidMount() {
-    console.log(this.props);
     this.getAlumno(this.props.id);
+  }
+
+  calcularCursoActual(alumno) {
+    if(alumno.esopres2 && !alumno.esodev2) return 1;
+    if(alumno.esopres3 && !alumno.esodev3) return 2;
+    if(alumno.esopres4 && !alumno.esodev4) return 3;
+    if(alumno.fpbpres1 && !alumno.fpbdev1) return 4;
+    if(alumno.fpbpres2 && !alumno.fpbdev2) return 5;
+    return 0;
   }
 
   getAlumno(id) {
@@ -25,6 +33,7 @@ class Alumno extends Component {
       .then((response) => {
         this.setState({
           currentAlumno: response.data,
+          cursoIndex: this.calcularCursoActual(response.data),
         });
         console.log(response.data);
       })
@@ -34,11 +43,48 @@ class Alumno extends Component {
   }
 
   render() {
-    const { currentAlumno } = this.state;
+    const { currentAlumno, cursoIndex } = this.state;
+
     return (
       <div>
-        <h1>{`Id: ${currentAlumno.id} Nombre: ${currentAlumno.nombre} Apellidos: ${currentAlumno.apellidos}`}</h1>
-        {currentAlumno.esopres2 ? "true" : "false"}
+        <h1>{`${currentAlumno.id} - ${currentAlumno.nombre} ${currentAlumno.apellidos}`}</h1>
+
+        <ul class="nav nav-tabs">
+          <li class="nav-item">
+            <a class={`nav-link ${cursoIndex===0?"active":""}`} onClick={()=>this.setState({cursoIndex: 0})} aria-current="page" href="#">1º ESO</a>
+          </li>
+          <li class="nav-item">
+            <a class={`nav-link ${cursoIndex===1?"active":""}`} onClick={()=>this.setState({cursoIndex: 1})} href="#">2º ESO</a>
+          </li>
+          <li class="nav-item">
+            <a class={`nav-link ${cursoIndex===2?"active":""}`} onClick={()=>this.setState({cursoIndex: 2})} href="#">3º ESO</a>
+          </li>
+          <li class="nav-item">
+            <a class={`nav-link ${cursoIndex===3?"active":""}`} onClick={()=>this.setState({cursoIndex: 3})} href="#">4º ESO</a>
+          </li>
+          <li class="nav-item">
+            <a class={`nav-link ${cursoIndex===4?"active":""}`} onClick={()=>this.setState({cursoIndex: 4})} href="#">1º FPB</a>
+          </li>
+          <li class="nav-item">
+            <a class={`nav-link ${cursoIndex===5?"active":""}`} onClick={()=>this.setState({cursoIndex: 5})} href="#">2º FPB</a>
+          </li>    
+        </ul>   
+        <div class="mb-3">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked />
+          <label class="form-check-label" for="flexCheckDefault">Préstamo realizado</label>
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">Observaciones préstamo</label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">-Todo en buen estado</textarea>
+        </div>     
+        <div class="mb-3">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+          <label class="form-check-label" for="flexCheckDefault">Devolución realizada</label>
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">Observaciones devolución</label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>           
       </div>
     );
   }
