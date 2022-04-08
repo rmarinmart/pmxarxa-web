e1ecf2c87a70f33fbb02abe87b087244d4f86813
rmmcoder <45757618+rmmcoder@users.noreply.github.com>
2022-04-08 10:35:21 +0200

    Busca el server en el mismo dominio del front

--- xarxa-web/src/http-common.js
+++ xarxa-web/src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: `http://${window.location.hostname}:8080/api`,
   headers: {
     "Content-type": "application/json",
   },
